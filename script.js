e1d21a5cdb5b6fe85c25b5a2b635e3852a671863
ALlen B <pingguo.ny@gmail.com>
2017-10-13 22:03:13 -0400

    Add basic functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 jsconsole = {};
 jsconsole.rawlog = function(str, color) {
-  var out = document.getElementById("out");
+  var out = document.getElementById("output");
   var line = document.createElement("div");
   line.style.color = color;
   line.appendChild(document.createTextNode(str));
@@ -8,11 +8,38 @@ jsconsole.rawlog = function(str, color) {
 }
 
 console.log = function(args) {
-  for(var i = 0; i < args.length; i++) {
-    jsconsole.rawlog(args[i]);
+  for(var i = 0; i < arguments.length; i++) {
+    jsconsole.rawlog(arguments[i]);
   }
 }
 
-document.getElementById("top").onclick = function() {
-  console.log(eval(this.value));
+console.warn = function(args) {
+  for(var i = 0; i < arguments.length; i++) {
+    jsconsole.rawlog(arguments[i], "#FA0");
+  }
+}
+
+console.error = function(args) {
+  for(var i = 0; i < arguments.length; i++) {
+    jsconsole.rawlog(arguments[i], "#F00");
+  }
+}
+
+window.onload = function() {
+  var input = document.getElementById("top");
+  input.focus()
+  input.onkeypress = function(e) {
+    if(e.keyCode == 13 || e.which == 13) {
+      tempval = this.value.trim();
+      this.value = "";
+      jsconsole.rawlog("> " + tempval, "#00A");
+      if(tempval) {
+        try {
+          jsconsole.rawlog(eval(tempval), "#00F");
+        } catch(err) {
+          jsconsole.rawlog(err, "#F00");
+        }
+      }
+    }
+  }
 }
